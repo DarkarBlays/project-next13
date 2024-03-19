3f0dfd8ed29367866102d51e3380ab1b334837b6
AnaMartinez0 <82622605+AnaMartinez0@users.noreply.github.com>
2024-03-19 00:40:44 -0500

    Finally finished

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cd66c54248aa2fc1"; }
+/******/ 		__webpack_require__.h = function() { return "03d4351f49761f5a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/change-register/page.tsx
+++ src/app/change-register/page.tsx
@@ -1,8 +1,59 @@
+"use client"
 
-export default function ChangePasswordPage() {
+import { Form } from "@/components/Form";
+import { useAuthFetch } from "@/hooks/useAuthFetch";
+import { useLoading } from "@/hooks/useLoading";
+import { AxiosRequestConfig } from "axios";
+import { useSearchParams } from "next/navigation";
+
+export default function LoginPage() {
+  const {finishLoading, isLoading, startLoading} = useLoading()
+  const searchParams = useSearchParams()
+  const AuthFetch = useAuthFetch()
+
+  const changePassword = async (formData: any) => {
+    startLoading()
+
+    const token = searchParams.get('token')
+
+    const options: AxiosRequestConfig<any> = {
+      headers: {
+        token
+      }
+    }
+
+    await AuthFetch({
+      endpoint: 'change-password',
+      formData,
+      redirectRoute: '/',
+      options
+    })
+    finishLoading()
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>ChangePasswordPage</h1>
-    </main>
+    <>
+      <Form
+        title="Cambiar contraseña" 
+        onSubmit={changePassword}
+        description="Formulario para cambiar contraseña"
+      >
+        <div className="my-[10px] flex flex-col gap-4">
+        <Form.Input
+            label="Contraseña"
+            name="password"
+            placeholder="Ingresa tu contraseña..."
+            type="password"
+        />
+        <Form.Input
+            label="Contraseña"
+            name="confirmPassword"
+            placeholder="Repite tu contraseña..."
+            type="password"
+        />
+        </div>
+        <Form.SubmitButton buttonText="Cambiar Contraseña" isLoading={isLoading}/>
+      </Form>
+    </>
   );
 }
